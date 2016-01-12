5964df17ae2edbdb22b18ad3654746a90a5580fa
Florian Engelmann <florian.engelmann@everyware.ch>
2016-01-12 16:55:53 +0100

    changed description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ license 'Apache 2.0'
 name 'maxscale'
 maintainer 'EveryWare'
 maintainer_email 'unix@everyware.ch'
-description 'Installs and configures maxscale 1.2.x'
+description 'installs and configures maxscale 1.2.x'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
 supports 'ubuntu'
